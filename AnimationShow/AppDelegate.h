b140a1a6817e9d4eb5b044a177eb62c50162dd12
zgpeace <zgpeace@gmail.com>
2015-08-17 10:17:54 +0800

    Initial Commit

---
+++ AnimationShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationShow
+//
+//  Created by cbz on 8/17/15.
+//  Copyright (c) 2015 zgpeace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationShow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationShow
+//
+//  Created by cbz on 8/17/15.
+//  Copyright (c) 2015 zgpeace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
